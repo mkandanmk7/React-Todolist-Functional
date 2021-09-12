f98b5a788afe28d30f46ff791647757f1038b1ab
muthu <mkandanmk7@gmail.com>
2021-09-12 22:14:51 +0530

    todo done

--- src/App.js
+++ src/App.js
@@ -26,7 +26,18 @@ function App() {
 
   let handleChange = ({ target: { name, value, type } }) => {
     setTodo(value);
-    console.log(name, value, type);
+    // console.log(name, value, type);
+
+    const regex = new RegExp(/^[_A-z0-9]*((-|\s)*[_A-z0-9])*$/g);
+    if (value.length < 15 || value.length >= 20) {
+      setError({ ...error, text: "words limit 15-20" });
+      console.log("error display", error.textarea);
+    } else if (!regex.test(value)) {
+      console.log("regex working");
+      setError({ ...error, textarea: "should not use Special charactors" });
+    } else {
+      setError({ ...error, textarea: "" });
+    }
   };
 
   // handleSubmit()
@@ -35,8 +46,23 @@ function App() {
     console.log("submited");
 
     event.preventDefault();
+
+    console.log(todo);
+
+    let obj = {
+      desc: todo,
+      date: new Date(),
+      completed: false,
+    };
+    setTodos([...todos, obj]);
+    setTodo(""); //empty the text-area
   };
 
+  let changeState = (data) => {
+    data.completed = !data.completed;
+
+    setTodos([...todos]);
+  };
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
@@ -52,6 +78,29 @@ function App() {
           <button type="submit">Add</button>
         </div>
       </form>
+      <div className="list">
+        <div className="listItem">
+          <ol>
+            {todos.map((a) => {
+              let button;
+
+              if (a.completed)
+                button = <i className="fas fa-check-circle tick"></i>;
+              else button = <i className="fas fa-wrong-circle wrong "></i>;
+
+              return (
+                <li key={a.desc.toString()}>
+                  <span className="btn" onClick={() => changeState(a)}>
+                    <h3>{button}</h3>
+                  </span>
+                  <span className="desc">{a.desc}</span>
+                  <span className="date">{a.date.toDateString()}</span>
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+      </div>
     </div>
   );
 }
